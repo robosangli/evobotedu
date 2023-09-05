26d19d488e818104b5b2a28106f699c2eafabb62
Ananda Sangli <anandasangli@Anandas-MBP.attlocal.net>
2023-09-05 14:12:38 -0700

    Finished Simulation.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # evobotedu
-This code repository is created by Ananda Sangli on September 4 2023 while following Josh Bongard's Education in Evolutionary Robotics Course. They run the Xenobot lab at Northwestern University.
+This code repository was created by Ananda Sangli on September 4 2023 while following Josh Bongard's Education in Evolutionary Robotics Course. Josh runs the Xenobot lab at Northwestern University.
+# Simulation
+An object was created to handle the physics (pybullet) and draw the results to a GUI simulation. The time library is used to step into the simulated world while slowing things down on the GUI.
--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)    # creates object 'physicsClient', handles physics, and draws results to GUI
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)      # disable pybullet sidebars resulting in speed-up of GUI simulation
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)      # disable pybullet sidebars resulting in speed-up of GUI simulation
 for i in range(0, 500):
     p.stepSimulation()              # stepping the world: slowing things down to see the simulated world
     print("for: ", i)               # to get a real-time sense of how long each loop iteration takes
