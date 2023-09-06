0bf01fded76dfcda26990823ff13c5800b412b68
Ananda Sangli <anandasangli@Anandas-MBP.attlocal.net>
2023-09-05 19:40:04 -0700

    Generating one link

---
+++ generate.py
@@ -0,0 +1,9 @@
+# use pyrosim to generate a link (later, several links comprising the world),
+# store it in a special file called world.sdf, and
+# simulate.py will then read it in and simulate it.
+import pyrosim.pyrosim as pyrosim       # Python Robot Simulator package communicates easily with Pybullet
+
+pyrosim.Start_SDF("box.sdf")            # information of world being created will be stored in 'box.sdf'
+# Note: SDF stands for Software Development Folder
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])     # stores box's initial position and dimensions into 'box.sdf'
+pyrosim.End()                           # closes the sdf file
