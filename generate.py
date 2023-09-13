d0a714eab448bcce59b46079d0ba663f470aac9c
Ananda Sangli <anandasangli@Anandas-MBP.attlocal.net>
2023-09-12 19:19:47 -0700

    Successfully generated a tower of 10 blocks.

--- generate.py
+++ generate.py
@@ -11,6 +11,8 @@ height = 1
 x = 0
 y = 0
 z = height/2.0                          # ensure that the bottom surface is right at ground level (& not embedded into the floor)
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])     # stores box's initial position and dimensions into 'boxes.sdf'
-pyrosim.Send_Cube(name="Box2", pos=[x + length,y,z + height] , size=[length,width,height])     # stores box2's initial position and dimensions
+# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])     # stores box's initial position and dimensions into 'boxes.sdf'
+# pyrosim.Send_Cube(name="Box2", pos=[x + length,y,z + height] , size=[length,width,height])     # stores box2's initial position and dimensions
+for i in range(0, 10):                  # using a for loop to procedurally generate each block in the tower
+    pyrosim.Send_Cube(name=("Box"+str(i)), pos=[x,y,z + (i*height)] , size=[length*(0.9**i),width*(0.9**i),height*(0.9**i)])
 pyrosim.End()                           # closes the sdf file
