49a7a683568acbd6c52562586463fd88978b66fc
Ananda Sangli <anandasangli@Anandas-MBP.attlocal.net>
2023-09-16 20:12:43 -0700

    Successfully simulated a minimal world.

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@
 # simulate.py will then read it in and simulate it.
 import pyrosim.pyrosim as pyrosim       # Python Robot Simulator package communicates easily with Pybullet
 
-pyrosim.Start_SDF("boxes.sdf")          # information of world being created will be stored in 'boxes.sdf'
+pyrosim.Start_SDF("world.sdf")          # information of world being created will be stored in 'world.sdf'
 # Note: SDF stands for Software Development Folder
 length = 1                              # in units of m
 width = 1
@@ -11,10 +11,5 @@ height = 1
 x = 0
 y = 0
 z = height/2.0                          # ensure that the bottom surface is right at ground level (& not embedded into the floor)
-# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])     # stores box's initial position and dimensions into 'boxes.sdf'
-# pyrosim.Send_Cube(name="Box2", pos=[x + length,y,z + height] , size=[length,width,height])     # stores box2's initial position and dimensions
-for i in range(0, 10):                  # using a for loop to procedurally generate each block in the tower
-    for j in range (0, 5):              # using a for loop to create 5 more towers in the 'y' dimension
-        for k in range (0, 5):          # using a for loop to create 5 more towers in the 'x' dimension
-            pyrosim.Send_Cube(name=("Box"+str(k)+str(j)+str(i)), pos=[x + k,y + j,z + (i*height)] , size=[length*(0.9**i),width*(0.9**i),height*(0.9**i)])
+pyrosim.Send_Cube(name="Block", pos=[x,y,z], size=[length,width,height])        # creates a simple block
 pyrosim.End()                           # closes the sdf file
--- simulate.py
+++ simulate.py
@@ -3,11 +3,11 @@ import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)    # creates object 'physicsClient', handles physics, and draws results to GUI
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)      # disable pybullet sidebars resulting in speed-up of GUI simulation
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)      # disable pybullet sidebars resulting in speed-up of GUI simulation
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)              # adds the force of gravity to exist in the world
 planeId = p.loadURDF("plane.urdf")  # adds a floor to the simulation from preexisting Pybullet data
-p.loadSDF("boxes.sdf")                # pybullet reads in the world described in 'box.sdf' (from generate.py)
+p.loadSDF("world.sdf")                # pybullet reads in the world described in 'world.sdf' (from generate.py)
 for i in range(0, 500):
     p.stepSimulation()              # stepping the world: slowing things down to see the simulated world
     print("for: ", i)               # to get a real-time sense of how long each loop iteration takes
